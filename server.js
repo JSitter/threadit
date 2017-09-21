5b65f31893a8c46a48bce94e3c0b003075625d35
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-20 22:39:16 -0700

    broken route

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+let hbs = require('express-handlebars');
 
-app.set('views', 'handlebars')
+const app = express();
+//Setup View Engine with configuration options
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout:"main"}));
+app.set('view engine', 'hbs');
+
+//app.use('/', )
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('main.hbs', "test");
+});
 
 app.get('/posts/new', function(req, res) {
-    res.render('./posts-new.handlebars')
-})
+    res.render('./views/layouts/layout.hbs');
+});
 
 app.listen(8082, function () {
-  console.log('Example app listening on port 8082!')
-})
+  console.log('Example app listening on port 8082!');
+});
