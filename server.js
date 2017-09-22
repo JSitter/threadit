26227730aa139a9ecfb5b2b76cdc20da5a40d596
Justin Sitter <justin@jaytria.com>
2017-09-22 09:18:06 -0700

    Code cleanup

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express')
-var exphbs = require('express-handlebars');
+var express = require('express');
+var hbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express();
 
-var app = express()
+//connect to threadit database
+mongoose.connect('localhost/threadit')
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}));
+//log database errors to console
+mongoose.connection.on('error', console.error.bind(console, "MongoDB Connection error"));
+
+//Setup Mongodb Posts Model
+var Post = mongoose.model('Post', {
+    title: String,
+    post_body: String,
+    user_name: String
+})
+
+//Add bodyParser to App
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Setup handlebars view engine and pass in parameters
+app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}));
 app.set('view engine', 'hbs');
 
+//Setup root landing page
 app.get('/', function (req, res) {
     res.render('posts-new', {msg: 'Hello World!'})
-    //res.send("hello")
-
 })
 
 app.listen(8082, function () {
